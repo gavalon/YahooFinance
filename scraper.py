8478448ae7f29cb62d2fd84a2b754f7c97aff9f9
gavalon <gavalon@stanford.edu>
2014-10-25 22:23:38 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,8 +2,15 @@ import urllib
 from urllib import urlopen
 import re
 
-url = "http://finance.yahoo.com/q?uhb=uh3_finance_vert&fr=&type=2button&s=AMZN%2C"
-f = urllib.urlopen(url)
-words = f.read().decode('utf-8')
-price = re.findall("<span id=\"yfs_l86_amzn\">(.+)</span></span> <span class=\"up_g\">", words)
-print price[0]
+stocks = ['pfe', 'bx', 'alxn', 'nflx', 'crm', 'yelp']
+url_addendum = '\%2C'
+base_url = "http://finance.yahoo.com/q?uhb=uh3_finance_vert&fr=&type=2button&s="
+for i in stocks:
+    url = base_url + i + url_addendum
+    f = urllib.urlopen(url)
+    words = f.read().decode('utf-8')
+    find_query_1 = "<span id=\"yfs_l86_"
+    find_query_2 = "\">(.+)</span></span> <span class=\""
+    query = find_query_1 + i + find_query_2
+    price = re.findall(query, words)
+    print i, price[0]
